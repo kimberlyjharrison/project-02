101224ece259f4253e8e1f14a67daf7b0b62cf23
kimberlyjharrison <kimberly.j.harrison@gmail.com>
2019-06-22 21:14:20 -0700

    functional prototype!!! need to fix map loading sequence, but it works!

---
+++ flask-app-functional/job-map/static/js/logic.js
@@ -0,0 +1,118 @@
+console.log('app.js working')
+
+//   reference:
+//https://www.codebyamir.com/blog/populate-a-select-dropdown-list-with-json
+let dropdown = $('#selDataset');
+
+  dropdown.empty();
+  
+  dropdown.append('<option selected="true" disabled>Choose Job Title</option>');
+  dropdown.prop('selectedIndex', 0);
+  
+  const url = '/jobs';
+  
+  // Populate dropdown with list of provinces
+  $.getJSON(url, function (data) {
+    $.each(data, function (key, entry) {
+      dropdown.append($('<option></option>').attr('onClick', "reply_click(this.id)").attr('value', entry.title).text(entry.label));
+    })
+});
+
+function optionChanged(newSample) {
+    // Fetch new data each time a new sample is selected
+    console.log(newSample)
+    var apiurl = 'http://127.0.0.1:5000/filter/'+newSample
+    var data = d3.json(apiurl, function(data) {
+    createMap(data)
+})
+}
+
+var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+});
+
+var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.dark",
+  accessToken: API_KEY
+});
+
+function createMap(data) {
+  var jobPostingsArray = [];
+  for (var i = 0; i < data.length; i++) {
+      var jobPosting = data[i];
+      console.log(jobPosting)
+      if (jobPosting.Coordinates && jobPosting.Location) {
+
+          jobPostingsArray.push(L.marker(jobPosting.Coordinates)
+              .bindPopup("<h2>" + jobPosting.Title + "</h2>" + "<h3>" + jobPosting.Company + "</h3> <hr> <h4>" + jobPosting.Location +
+              "</h4> <h5>" + jobPosting.Salary_Info + "</h5> <a href=\"" + jobPosting.Link + "\" target=\"_blank\">Find Job Posting Here</a>"));
+      }
+  };
+
+  var heatArray = [];
+  for (var i = 0; i < data.length; i++) {
+      var jobPosting = data[i];
+
+      if (jobPosting.Location) {
+        heatArray.push(jobPosting.Coordinates);
+      };
+
+      var heat = L.heatLayer(heatArray, {
+        radius: 80,
+        blur: 28,
+        minOpacity: 0.55
+      });
+  };
+
+  var zillowArray = [];
+  for (var i = 0; i < zillow.length; i++) {
+
+      if (zillow[i].lat && zillow[i].lng) {
+          zillowArray.push([zillow[i].lat, zillow[i].lng])
+      }
+
+      var zillowHeat = L.heatLayer(zillowArray, {
+          radius: 10,
+          blur: 1,
+          minOpacity: 0.2,
+          gradient: {
+              '.2': 'blue',
+              '.4': 'green',
+              '.6': 'yellow',
+              '.8': 'orange',
+              '.99': 'red'
+          },
+      });
+  };
+
+
+  jobPostingsLayer = L.layerGroup(jobPostingsArray);
+
+
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+  };
+
+  var overlayMaps = {
+    "Job Postings": jobPostingsLayer,
+    "HeatMap of Job Posts": heat,
+    "Median $/sqft (Housing)": zillowHeat
+  };
+
+
+  var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5,
+    layers: [streetmap, jobPostingsLayer]
+  });
+
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+}
